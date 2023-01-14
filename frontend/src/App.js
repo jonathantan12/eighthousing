807eb023a15f978652bac3b67a7a7b4962f0a674
jonathantan12 <74011226+jonathantan12@users.noreply.github.com>
2023-01-14 18:49:05 +0800

    stopped here

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,62 @@
 // import './App.css';
 
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import {Routes, Route, Link} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import HousingList from "./components/housing-list";
+import Login from "./components/login";
+
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
+
   return (
+
     <div className="App">
-      Hello World.
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/housing-list" className="navbar-brand">
+          Housing List
+        </a>
+
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/housing-list"} className="nav-link">
+              Housing
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            {user ? (
+              <a onClick={logout} className="nav-link" style={{cursor:'point'}} href="/#">
+                Logout {user.name}
+              </a>
+             ) : (
+              // False
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link> 
+            )}
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Routes>
+          <Route exact path={["/", "/housing-list"]} component={HousingList} /> 
+          <Route path="/login" render={(props) => (
+            <Login {...props} login={login} />
+          )} 
+          />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/housing-list.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HousingList() {
+  return (
+    <div className="App">
+      Hello World.
+    </div>
+  );
+}
+
+export default HousingList;
